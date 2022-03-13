b1d3fcee26232289fda630555916438c04f3511b
IversonLeitzke <iversonleitzke@gmail.com>
2022-03-13 14:04:39 -0300

    Adding Readme

---
+++ CollegeExercises/FirstExecises/Two/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for (int c = 1; c <= 6; c++) {
+            for (int counter = 1; counter <= c; counter++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# java
+
+**Java - Como Programar - Deitel & Deitel - 10º Edição // Exercises from the book**
+
+**College Execises**
+
+1. Printing POO with asterisks, printing a "\*" for it iteration of the loop, converting Celsius in Fahrenheit
