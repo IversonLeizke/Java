085e0070390f45449e56b6a3425fb68d5e15f4c5
IversonLeitzke <iversonleitzke@gmail.com>
2022-05-08 15:56:35 -0300

    Polimorfismo

---
+++ CollegeExercises/Polimorfismo/Calculadora/src/Divisao.java
@@ -0,0 +1,7 @@
+public class Divisao extends OperacaoMatematica {
+
+    public double calcular(double x, double y) {
+        return x / y;
+    }
+    
+}
---
+++ CollegeExercises/Polimorfismo/Calculadora/src/Subtracao.java
@@ -0,0 +1,7 @@
+public class Subtracao extends OperacaoMatematica {
+
+        public double calcular(double x, double y) {
+            return x - y;
+        }
+
+}
